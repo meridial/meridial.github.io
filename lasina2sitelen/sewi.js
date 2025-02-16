a6542623f6e54240838f3a089250f253cd236a8e
meridial <meridial@localhost.localdomain>
2025-02-16 18:53:25 +0700

    font size bullshit

--- lasina2sitelen/sewi.js
+++ lasina2sitelen/sewi.js
@@ -1,8 +1,8 @@
 update_sitelen = () =>{
-    v = document.getElementById("lasina_input").value
+    const v = String(document.getElementById("lasina_input").value)
     if (v == ""){
         x = document.getElementById("out_p").innerText = "sitelen li kama lon ni"
         return
     }
-    document.getElementById("out_p").innerText = v
+    document.getElementById("out_p").innerText = v.trim()
 }
